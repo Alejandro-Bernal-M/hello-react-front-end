8d45ff0e043d762fe34cc7df0fbec9f8e3d3851d
alejandro <alejandrober96@gmail.com>
2023-07-11 12:44:29 -0500

    Greeting route working with redux

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchGreeting } from "../redux/slices/greetingSlice";
+
+const Greeting = () => {
+  const dispatch = useDispatch();
+  const { loading, message } = useSelector((state) => state.greeting);
+
+  useEffect(() => {
+    dispatch(fetchGreeting());
+  }, [dispatch]);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <h1>{message}</h1>
+  )
+}
+
+export default Greeting;
