0254d3ef1be7029830038fbe9ad2c0fac2991400
alejandro <alejandrober96@gmail.com>
2023-07-11 13:12:08 -0500

    fix-linters

--- src/redux/slices/greetingSlice.js
+++ src/redux/slices/greetingSlice.js
@@ -1,34 +1,34 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
-export const fetchGreeting = createAsyncThunk('greeting/fetchGreeting' , async () => {
-    const response = await fetch('http://localhost:3000/api/v1/greetings');
-    const data = await response.json();
-    const greeting = data.message;
-    return greeting;
-} )
+export const fetchGreeting = createAsyncThunk('greeting/fetchGreeting', async () => {
+  const response = await fetch('http://localhost:3000/api/v1/greetings');
+  const data = await response.json();
+  const greeting = data.message;
+  return greeting;
+});
 
 const initialState = {
-    message: '',
-    loading: false
-}
+  message: '',
+  loading: false,
+};
 
 const greetingSlice = createSlice({
-    name: 'greeting',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-      builder.addCase(fetchGreeting.pending, (state) => {
-        state.loading = true;
-      })
-      builder.addCase(fetchGreeting.fulfilled, (state, action) => {
-        state.message = action.payload;
-        state.loading = false;
-      })
-      builder.addCase(fetchGreeting.rejected, (state) => {
-        state.loading = false;
-        state.message = 'Error';
-      })
-      }
-})
+  name: 'greeting',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchGreeting.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(fetchGreeting.fulfilled, (state, action) => {
+      state.message = action.payload;
+      state.loading = false;
+    });
+    builder.addCase(fetchGreeting.rejected, (state) => {
+      state.loading = false;
+      state.message = 'Error';
+    });
+  },
+});
 
 export default greetingSlice.reducer;
